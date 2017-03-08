f54972e456108224ebcc65ffc33ea6ef7df01f2c
fran-aguilar <fran.kempes@gmail.com>
2017-03-08 18:28:01 +0100

    added demo20 THREEDC example.

--- js/func.js
+++ js/func.js
@@ -43,6 +43,7 @@ AFRAMEDC.Panel = function (dashBoard, coords, numberOfCharts, size, opacity) {
     if (!(dashBoard instanceof AFRAMEDC.DashBoard)) return;
     var aux_coords = coords || [0, 0, 0];
     this.parentDashBoard = dashBoard;
+   
     this.coords = new THREE.Vector3( aux_coords[0], aux_coords[1], aux_coords[2] );
     this.numberOfCharts = numberOfCharts || 4;
     this.opacity = opacity || 0.3;
@@ -98,12 +99,13 @@ AFRAMEDC.Panel = function (dashBoard, coords, numberOfCharts, size, opacity) {
         if (numberOfAnchorPoints === 2) {
             anchorPoints.push({
                 filled: false,
-                coords: new THREE.Vector3(panel.coords.x - panel._size[0] / 2, panel.coords.y - panel._size[1] / 2, panel.coords.z)
+                coords: new THREE.Vector3(PADDING - panel._size[0] / 2, PADDING - panel._size[1] /2, 0)
             });
             anchorPoints.push({
                 filled: false,
-                coords: new THREE.Vector3(panel.coords.x - panel._size[0] / 2, panel.coords.y, panel.coords.z)
+                coords: new THREE.Vector3(PADDING, PADDING - panel._size[1] /2, 0)
             });
+
         }
         panel.anchorPoints = anchorPoints;
     };
@@ -119,8 +121,13 @@ AFRAMEDC.Panel = function (dashBoard, coords, numberOfCharts, size, opacity) {
     };
     this.getChartSize = function () {
         //calculate inner bounds.
-        return [(this._size[0] / 2) - PADDING * 2, (this._size[1] / 2) - PADDING * 2];
+        if (this.numberOfCharts === 4) {
+            return [(this._size[0] / 2) - PADDING * 2, (this._size[1] / 2) - PADDING * 2];
+        } else if (this.numberOfCharts === 2) {
+            return [(this._size[0] / 2) - PADDING * 2, (this._size[1]  ) - PADDING * 2];
+        }
     };
+    dashBoard.allPanels.push(this);
 };
 //build DOM Element.
 AFRAMEDC.Panel.prototype.build = function () {
@@ -330,7 +337,14 @@ AFRAMEDC.BasicChart.prototype.color = function (color) {
         console.log("argument needed");
         return;
     }
-    this._color = color;
+    
+    if (typeof (color) === "number") {
+        //transform color to string hex.
+        var aux_color = "#" + ("000000" + color.toString(16)).slice(-6);
+        this._color = aux_color;
+    }else  {
+        this._color = color;
+    };
     return this;
 };
 
@@ -411,6 +425,46 @@ AFRAMEDC.BasicChart.prototype.getEscene = function () {
     if (this._parentDashBoard)
         return this._paretnDashBoard.scene;
 };
+AFRAMEDC.BasicChart.prototype.sortCFData = function () {
+    var unsort_data = this._group.top(Infinity);
+
+    var dates = [];
+    //en dates guardo las fechas(keys)
+    for (var i = 0; i < unsort_data.length; i++) {
+        dates[i] = unsort_data[i].key;
+    };
+    //ordeno fechas(keys) de menor a mayor
+    dates.sort(function (a, b) { return a - b });
+
+    //ordeno el grupo de menor a mayor usando
+    //las posiciones de dates
+    var _data = [];
+    for (var i = 0; i < dates.length; i++) {
+        for (var j = 0; j < unsort_data.length; j++) {
+            if (dates[i] === unsort_data[j].key) {
+                _data[i] = {
+                    key: unsort_data[j].key,
+                    value: unsort_data[j].value
+                };
+            }
+        };
+    };
+
+    return _data;
+};
+AFRAMEDC.BasicChart.prototype.tooltip = function (tooltipf) {
+    if (!tooltipf) {
+        console.log("arg needed");
+        return;
+    }
+    if (typeof tooltipf !== "function") {
+        console.log("arg needed");
+        return;
+    }
+    this._tooltip = tooltipf;
+    return this;
+
+}
 AFRAMEDC.BasicChart.prototype.addEvents = function () {
     var addEvent = function (basicChart, partElement) {
         partElement.DOMElement.addEventListener('mouseenter', partEventsEnter.bind(basicChart, partElement));
@@ -445,7 +499,7 @@ AFRAMEDC.BasicChart.prototype.addEvents = function () {
         texto.setAttribute("text", {
         color:  "#000000",
             side: "double",
-            value: el.name,
+            value: basicChart._tooltip ? basicChart._tooltip(el.data) : el.name,
             width: TEXT_WIDTH,
             wrapCount: 30
         });
@@ -547,8 +601,6 @@ AFRAMEDC.PieChart.prototype.build = function (location) {
         !this._group ) return;
     var relativeX, relativeY, relativeZ;
     var thethainit = 0;
-    var FONT_HEIGHT = 2;
-    var MIN_DISTANCE = (0.01 + 0.04615) * FONT_HEIGHT;
     var COLORS = ['#2338D9', '#23A2D9', '#23D978', '#BAD923', '#D923D3', '#23D7D7', '#D72323', '#262C07'];
     var entityEl = document.createElement("a-entity");
     var radius = Math.min(this._width / 2, this._height / 2);
@@ -602,7 +654,7 @@ AFRAMEDC.PieChart.prototype.build = function (location) {
                 key: _data[j].key,
                 value: _data[j].value
             },
-            position: { x: relativeX, y: relativeY + this._width / 2 + 0.25, z: relativeZ },
+            position: { x: relativeX, y: relativeY + radius + 0.25, z: relativeZ },
             origin_color: actualColor
         };
         piePart.DOMElement = el;
@@ -664,7 +716,7 @@ AFRAMEDC.BarsChart.prototype.build = function () {
     if (this._data && this._data.length > 0) {
         _data = this._data;
     } else if (this._group){
-        _data = this._group.top(Infinity);
+        _data = this.sortCFData();
     }
     var dataValues = _data.map(function (a) { return a.value; });;
     dataValues = AFRAMEDC.utils.scale.apply(null, dataValues);
@@ -684,7 +736,7 @@ AFRAMEDC.BarsChart.prototype.build = function () {
         var myHeight = dataValues[i] * MAX_HEIGHT;
         var myYPosition = __calculateY(relativeY, myHeight) ;
         var el = document.createElement('a-box');
-        var actualColor = COLORS[i % COLORS.length];
+        var actualColor =this._color || COLORS[i % COLORS.length];
         var elPos = { x: relativeX, y: myYPosition, z: relativeZ  };
 
         el.setAttribute('width', BAR_WIDTH);
---
+++ js/script-demo20.js
@@ -0,0 +1,85 @@
+﻿
+//JSON data saved here
+var json_data;
+
+//CROSSFILTER VARS
+
+var cf;
+
+var dimByMonth;
+
+var groupByMonth;
+
+var dimByOrg;
+
+var groupByOrg;
+
+// initialization
+//getJSON call, draw meshes with data
+$.getJSON("data/scm-commits.json", function (data) {
+    json_data = data;
+    init();
+});
+function init() {
+
+    var parsed_data = [];
+
+    // Crossfilter and dc.js format
+    json_data.values.forEach(function (value) {
+        var record = {}
+        json_data.names.forEach(function (name, index) {
+            if (name == "date") {
+                var date = new Date(value[index] * 1000);
+                record[name] = date;
+                record.month = new Date(date.getFullYear(), date.getMonth(), 1);
+                record.hour = date.getUTCHours();
+            } else {
+                record[name] = value[index];
+            }
+        });
+        parsed_data.push(record);
+    });
+    cf = crossfilter(parsed_data);
+    var scene = document.querySelector('a-scene');
+    dimByMonth = cf.dimension(function (p) { return p.month; });
+
+    groupByMonth = dimByMonth.group();
+
+    //create a dimension by org
+
+    dimByOrg = cf.dimension(function (p) { return p.org; });
+
+    groupByOrg = dimByOrg.group();
+
+
+    //create a dimension by author
+
+    dimByAuthor = cf.dimension(function (p) { return p.author; });
+
+    groupByAuthor = dimByAuthor.group();
+
+    //CUSTOM DASHBOARD//
+    var dashBoard = new AFRAMEDC.DashBoard(scene);
+    var panel = new AFRAMEDC.Panel(dashBoard, [5, 0, 0], 2).size([25, 7]);
+    var bars = new AFRAMEDC.PieChart(panel);
+    bars.group(groupByOrg)
+       .dimension(dimByOrg)
+       .depth(2);
+    //.radius(100);
+    var line = new AFRAMEDC.BarsChart(panel);
+    line.group(groupByMonth)
+   .dimension(dimByMonth)
+   //.width(400)
+   //.height(200)
+   .numberOfXLabels(5)
+   .numberOfYLabels(5)
+   .gridsOn()
+   .tooltip(function (d) {
+       return "key: " + d.key.toLocaleString() + " value: " + d.value;
+   })
+   .depth(2);
+   //.color(0xff0000);
+
+
+    dashBoard.renderAll();
+};
